bd88b454d930fd9378e1b8b2978a4496e1526ecf
Daniel Dawson <dannyldawson@gmail.com>
2018-11-11 23:25:08 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,9 @@
 def fizzbuzz(int)
   if int % 5 == 0 && int % 3 == 0
      "FizzBuzz"
-  elsif int % 3 == 0
+  elseif int % 3 == 0
      "Fizz"
-  elsif int % 5 == 0 
+  elseif int % 5 == 0 
      "Buzz"
   end
 end
